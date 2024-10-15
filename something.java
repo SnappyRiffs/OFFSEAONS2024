8974045d64ea899183c69ee850abc494d07c1fc2
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-10-15 00:06:39 +0000

    Added code

--- something.java
+++ something.java
@@ -1,5 +1,12 @@
 public class something{
 	public static void main(String[] args){
-		System.out.println("Hello!");
+	
+	}
+	public int idk = 255;
+	public void setValue(int val){
+		this.idk = val;
+	}
+	public int getValue(){
+		return idk;
 	}
 }

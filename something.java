73432989ce7e75f29dae5aa3733a4ec81d5aaad1
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-10-28 23:33:03 +0000

    added hex, octal, bin and dec

--- something.java
+++ something.java
@@ -2,7 +2,7 @@ public class something{
 	public static void main(String[] args) {
 		System.out.println((char) 041); //octal works too
 		System.out.println((char) 0x21); //hex works too
-		System.out.println((char) 33); //octal works too
+		System.out.println((char) 33); 
 		System.out.println((char) 0b100001); //binary works too
 	}
 }

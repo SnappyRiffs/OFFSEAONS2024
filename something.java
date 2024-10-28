d435b1d6a64a175eac722dc531a3e37ed04a2282
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-10-28 22:44:05 +0000

    idk

--- something.java
+++ something.java
@@ -1,15 +1,3 @@
 public class something{
-	public int idk = 255;
-	public something(int idk){
-		this.idk = idk;
-	}
-	public something(){
-		this.idk = 0;
-	}
-	public void setValue(int val){
-		this.idk = val;
-	}
-	public static int getValue(){
-		return idk;
-	}
+	
 }

7a2a2207424f543e7b3e7843816eae5a3d0f0672
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-10-31 08:29:34 -0700

    added additional comments

--- something.java
+++ something.java
@@ -1,7 +1,7 @@
 public class something{
 	public static void main(String[] args) {
-		System.out.println((char) 041); //octal works too
-		System.out.println((char) 0x21); //hex works too
+		System.out.println((char) 041); //octal works too; java is really werid about their octal syntax: python uses 0o; c++ and other c related uses a pre-leading zero
+		System.out.println((char) 0x21); //hex works too; a-f can be either upper or lower case
 		System.out.println((char) 32); 
 		System.out.println((char) 0b100001); //binary works too
 	}

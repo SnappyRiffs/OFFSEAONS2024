44c54b2ab063adb4e1f8c283660e00585085ac18
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-10-31 08:31:30 -0700

    added additional comments
    octal syntax java

--- something.java
+++ something.java
@@ -1,6 +1,6 @@
 public class something{
 	public static void main(String[] args) {
-		System.out.println((char) 041); //octal works too
+		System.out.println((char) 041); //octal works too; 0o is python, other langauges uses 0<base-8 number>
 		System.out.println((char) 0x21); //hex works too
 		System.out.println((char) 32); 
 		System.out.println((char) 0b100001); //binary works too
